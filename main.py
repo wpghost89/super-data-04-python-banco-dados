cae8c9ff6d6b3bbec84b584126d1b3ea0da61a3d
Wpghhost89 <galego1989@gmail.com>
2026-09-17 21:56:56 -0300

    Aula 08

--- main.py
+++ main.py
@@ -98,3 +98,19 @@ Digite o menu desejado:"""
             print("Opção inválida!")
 
         menu_escolhido = int(input(menu))
+
+# Criar uma tabela de clientes com duas colunas id e nome
+# Inserir dois registros na tabela de clientes
+# Criar a função cadastrar_cliente no main.py
+#   Implementar a lógica de pedir o nome do cliente e cadastrar 
+#   o cliente com python e mysql
+#   Adicionar no menu principal para usuário poder chamar o cadastro do cliente
+# Criar a função apagar_cliente no main.py e implementar
+# Criar a função editar_cliente no main.py e implementar
+# Criar a função consultar_clientes no main.py e implementar
+# Alterar a tabela de clientes adicionando a coluna de cnpj
+# Modificar a função de cadastrar_cliente para solicitar o cnpj do 
+#   cliente e cadastrar com o cnpj
+# Modificar todas as demais funções de editar e consultar
+# Alterar a tabela de clientes adicionando a coluna de endereço, telefone, email, limite_credito
+# Modificar as funções de cadastro, editar e consultar
